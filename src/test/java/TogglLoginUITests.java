50e403cc62e68f83410033ec1a9e88b1e70e540c
a-pilipchuk <hanna.pilipchuk@gmail.com>
2021-01-08 18:59:21 +0300

    add negative test and video attachments

--- src/test/java/TogglLoginUITests.java
+++ src/test/java/TogglLoginUITests.java
@@ -3,18 +3,26 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.remote.DesiredCapabilities;
 
+import static com.codeborne.selenide.Browsers.FIREFOX;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.logevents.SelenideLogger.addListener;
 import static helpers.AttachmentsHelper.*;
 import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class ToggleLoginUITests {
+public class TogglLoginUITests {
     @BeforeAll
     static void setup(){
         addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
-        Configuration.browserSize= "1400x900";
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud:4444/wd/hub/";
+        Configuration.browserSize = "1400x1000";
+        //Configuration.browser = FIREFOX;
     }
 
     @AfterEach
@@ -22,13 +30,14 @@ public class ToggleLoginUITests {
         attachScreenshot("");
         attachPageSource();
         attachAsText("browser console log", getConsoleLogs());
+        attachVideo();
         closeWebDriver();
     }
 
     @Tag("UI_test")
     @Test
     @DisplayName("Successful login")
-    void ToggleLoginTest() {
+    void TogglPositiveLoginTest() {
         //step("", () ->{});
         step("open login page", () ->{
             open("https://toggl.com/track/login/");
@@ -45,6 +54,25 @@ public class ToggleLoginUITests {
         step("assert current URL equals expected URL", () ->{
             System.out.println(WebDriverRunner.url());
             assertEquals(WebDriverRunner.url(),"https://track.toggl.com/timer");
+        });
+    }
+
+    @Tag("UI_test")
+    @Test
+    @DisplayName("Unsuccessful login")
+    void TogglNegativeLoginTest() {
+        //step("", () ->{});
+        step("open login page", () ->{
+            open("https://toggl.com/track/login/");
+        });
+        step("fill in email field", () ->{
+            $("#email").val("qa.brest+1@gmail.com");
+        });
+        step("fill in password field with incorrect password and confirm", () ->{
+            $("#password").val("1").pressEnter();
+        });
+        step("assert that 'timer' is visible on the page", () ->{
+            $("a[href='/timer']").shouldBe(Condition.visible);
         });
     }
 }
--- src/test/java/helpers/AttachmentsHelper.java
+++ src/test/java/helpers/AttachmentsHelper.java
@@ -4,6 +4,7 @@ import com.codeborne.selenide.Selenide;
 import io.qameta.allure.Attachment;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.remote.RemoteWebDriver;
 
 import java.nio.charset.StandardCharsets;
 
@@ -28,5 +29,20 @@ public class AttachmentsHelper {
     @Attachment(value = "Page source", type = "text/plain")
     public static byte[] attachPageSource() {
         return getWebDriver().getPageSource().getBytes(StandardCharsets.UTF_8);
+    }
+
+    @Attachment(value = "Video", type = "text/html", fileExtension = ".html")
+    public static String attachVideo() {
+        return "<html><body><video width='100%' height='100%' controls autoplay><source src='"
+                + getVideoUrl()
+                + "' type='video/mp4'></video></body></html>";
+    }
+
+    public static String getVideoUrl() {
+        return "https://selenoid.autotests.cloud/video/" + getSessionId() + ".mp4";
+    }
+
+    public static String getSessionId(){
+        return ((RemoteWebDriver) getWebDriver()).getSessionId().toString();
     }
 }
